1707f5b7b6ae6b1245d20a72c30fe9c6c7ee53e5
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2023-01-04 17:32:01 +0100

    alibi metrics

--- scripts/sort_stats.R
+++ scripts/sort_stats.R
@@ -14,45 +14,57 @@ alibi_yml <- args[4]
 stats_pdf <- args[5]
 stats_csv <- args[6]
 
-#odgi_Y_yml <- "/home/heumos/Downloads/hogwild_paper/1D/odgi_Y/DRB1-3123.fa.gz.seqwish.gfa.Y.og.stats.yml"
-odgi_Y <- read_yaml(odgi_Y_yml, fileEncoding = "UTF-8", text, readLines.warn=TRUE)
+#odgi_Y_yml <- "/home/heumos/Downloads/hogwild_paper/1D/DRB1-3123/odgi_Y/DRB1-3123.fa.gz.seqwish.gfa.Y.og.stats.yml"
+odgi_Y <- read_yaml(odgi_Y_yml, readLines.warn=TRUE)
 odgi_Y_stats <- c(odgi_Y$mean_links_length[[1]]$length$in_node_space,
                   odgi_Y$mean_links_length[[1]]$length$in_nucleotide_space,
                   odgi_Y$sum_of_path_node_distances[[1]]$distance$in_node_space,
                   odgi_Y$sum_of_path_node_distances[[1]]$distance$in_nucleotide_space,
-                  "odgi Y")
+                  paste(odgi_Y$weighted_feedback_arc, "0", sep = "."),
+                  paste(odgi_Y$weighted_reversing_join, "0", sep = "."),
+                  "odgi_Y")
 odgi_Y_stats <- t(data.frame(odgi_Y_stats))
-colnames(odgi_Y_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", "sopnd_in_nuc_space", "tool")
+colnames(odgi_Y_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", 
+                            "sopnd_in_nuc_space", "weighted_feedback_arc", "weighted_reversing_join", "tool")
 
-#odgi_s_yml <- "/home/heumos/Downloads/hogwild_paper/1D/odgi_s/DRB1-3123.fa.gz.seqwish.gfa.s.og.stats.yml"
+#odgi_s_yml <- "/home/heumos/Downloads/hogwild_paper/1D/DRB1-3123/odgi_s/DRB1-3123.fa.gz.seqwish.gfa.s.og.stats.yml"
 odgi_s <- read_yaml(odgi_s_yml, fileEncoding = "UTF-8", text, readLines.warn=TRUE)
 odgi_s_stats <- c(odgi_s$mean_links_length[[1]]$length$in_node_space,
                   odgi_s$mean_links_length[[1]]$length$in_nucleotide_space,
                   odgi_s$sum_of_path_node_distances[[1]]$distance$in_node_space,
                   odgi_s$sum_of_path_node_distances[[1]]$distance$in_nucleotide_space,
-                  "odgi s")
+                  paste(odgi_s$weighted_feedback_arc, "0", sep = "."),
+                  paste(odgi_s$weighted_reversing_join, "0", sep = "."),
+                  "odgi_s")
 odgi_s_stats <- t(data.frame(odgi_s_stats))
-colnames(odgi_s_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", "sopnd_in_nuc_space", "tool")
+colnames(odgi_s_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", 
+                            "sopnd_in_nuc_space", "weighted_feedback_arc", "weighted_reversing_join", "tool")
 
-#odgi_Ygs_yml <- "/home/heumos/Downloads/hogwild_paper/1D/odgi_Ygs/DRB1-3123.fa.gz.seqwish.gfa.Ygs.og.stats.yml"
+#odgi_Ygs_yml <- "/home/heumos/Downloads/hogwild_paper/1D/DRB1-3123/odgi_Ygs/DRB1-3123.fa.gz.seqwish.gfa.Ygs.og.stats.yml"
 odgi_Ygs <- read_yaml(odgi_Ygs_yml, fileEncoding = "UTF-8", text, readLines.warn=TRUE)
 odgi_Ygs_stats <- c(odgi_Ygs$mean_links_length[[1]]$length$in_node_space,
                     odgi_Ygs$mean_links_length[[1]]$length$in_nucleotide_space,
                     odgi_Ygs$sum_of_path_node_distances[[1]]$distance$in_node_space,
                     odgi_Ygs$sum_of_path_node_distances[[1]]$distance$in_nucleotide_space,
-                    "odgi Ygs")
+                    paste(odgi_Ygs$weighted_feedback_arc, "0", sep = "."),
+                    paste(odgi_Ygs$weighted_reversing_join, "0", sep = "."),
+                    "odgi_Ygs")
 odgi_Ygs_stats <- t(data.frame(odgi_Ygs_stats))
-colnames(odgi_Ygs_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", "sopnd_in_nuc_space", "tool")
+colnames(odgi_Ygs_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", 
+                              "sopnd_in_nuc_space", "weighted_feedback_arc", "weighted_reversing_join", "tool")
 
-#alibi_yml <- "/home/heumos/Downloads/hogwild_paper/1D/alibi/DRB1-3123.fa.gz.seqwish.gfa.alibi.og.stats.yml"
-alibi <- read_yaml(alibi_yml, fileEncoding = "UTF-8", text, readLines.warn=TRUE)
+#alibi_yml <- "/home/heumos/Downloads/hogwild_paper/1D/DRB1-3123/alibi/DRB1-3123.fa.gz.seqwish.gfa.alibi.og.stats.yml"
+alibi <- read_yaml(alibi_yml, readLines.warn=TRUE)
 alibi_stats <- c(alibi$mean_links_length[[1]]$length$in_node_space,
                  alibi$mean_links_length[[1]]$length$in_nucleotide_space,
                  alibi$sum_of_path_node_distances[[1]]$distance$in_node_space,
                  alibi$sum_of_path_node_distances[[1]]$distance$in_nucleotide_space,
+                 paste(alibi$weighted_feedback_arc, "0", sep = "."),
+                 paste(alibi$weighted_reversing_join, "0", sep = "."),
                  "alibi")
 alibi_stats <- t(data.frame(alibi_stats))
-colnames(alibi_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", "sopnd_in_nuc_space", "tool")
+colnames(alibi_stats) <- c("mll_in_node_space", "mll_in_nuc_space", "sopnd_in_node_space", 
+                           "sopnd_in_nuc_space", "weighted_feedback_arc", "weighted_reversing_join", "tool")
 
 #layout <- rbind(odgi, BandageNG, Bandage, gfaviz)
 stats <- as.data.frame(rbind(odgi_Y_stats, odgi_s_stats, odgi_Ygs_stats, alibi_stats))
@@ -60,6 +72,9 @@ stats$mll_in_node_space <- as.numeric(stats$mll_in_node_space)
 stats$mll_in_nuc_space <- as.numeric(stats$mll_in_nuc_space)
 stats$sopnd_in_node_space <- as.numeric(stats$sopnd_in_node_space)
 stats$sopnd_in_nuc_space <- as.numeric(stats$sopnd_in_nuc_space)
+stats <- transform(stats, weighted_feedback_arc = as.numeric(weighted_feedback_arc), 
+          weighted_reversing_join = as.numeric(weighted_reversing_join))
+# is.numeric(stats$weighted_feedback_arc) https://stackoverflow.com/questions/2288485/how-to-convert-a-data-frame-column-to-numeric-type
 
 s_p_mll_node <- ggplot(stats, aes(x=tool, y=mll_in_node_space, fill=tool, color = tool, group=tool)) +
   geom_point() + geom_line() +
@@ -132,13 +147,43 @@ legend <- get_legend(s_p_sopnd_node)
 s_p_sopnd_node <- s_p_sopnd_node + theme(legend.position = "none")
 s_p_sopnd_node
 
-#### TODO ####
-#### CONTINUE HERE ####
-grid.arrange(arrangeGrob(s_p_mll_node, s_p_mll_nuc, s_p_sopnd_node, s_p_sopnd_nuc, nrow = 2), nrow = 2, heights = c(1,0.05))
+w_f_a <- ggplot(stats, aes(x=tool, y=weighted_feedback_arc, fill=tool, color = tool, group=tool)) +
+  geom_point() + geom_line() +
+  #geom_beeswarm(size=0.5) +
+  #geom_violin() +
+  labs(x = "tool", y = "weighted feedback arc") +
+  expand_limits(x = 0, y = 0) + theme(legend.direction = "horizontal") + 
+  labs(color = "tool   ") + 
+  labs(fill = "tool   ") 
+#theme(legend.title = element_text(size=15)) +
+#theme(legend.key.size = unit(0.4, 'cm'))
+w_f_a
 
-stats_save <- arrangeGrob(arrangeGrob(s_p_mll_node, s_p_mll_nuc, s_p_sopnd_node, s_p_sopnd_nuc, nrow = 2), nrow = 2, heights = c(1,0.05))
+legend <- get_legend(w_f_a)
+w_f_a <- w_f_a + theme(legend.position = "none")
+w_f_a
+
+w_r_j <- ggplot(stats, aes(x=tool, y=weighted_reversing_join, fill=tool, color = tool, group=tool)) +
+  geom_point() + geom_line() +
+  #geom_beeswarm(size=0.5) +
+  #geom_violin() +
+  labs(x = "tool", y = "weighted reversing join") +
+  expand_limits(x = 0, y = 0) + theme(legend.direction = "horizontal") + 
+  labs(color = "tool   ") + 
+  labs(fill = "tool   ") 
+#theme(legend.title = element_text(size=15)) +
+#theme(legend.key.size = unit(0.4, 'cm'))
+w_r_j
+
+legend <- get_legend(w_r_j)
+w_r_j <- w_r_j + theme(legend.position = "none")
+w_r_j
+
+grid.arrange(arrangeGrob(s_p_mll_node, s_p_mll_nuc, s_p_sopnd_node, s_p_sopnd_nuc, w_f_a, w_r_j, nrow = 3), nrow = 2, heights = c(1,0.05))
+
+stats_save <- arrangeGrob(arrangeGrob(s_p_mll_node, s_p_mll_nuc, s_p_sopnd_node, s_p_sopnd_nuc, w_f_a, w_r_j, nrow = 3), nrow = 2, heights = c(1,0.05))
 #stats_pdf <- "/home/heumos/Downloads/hogwild_paper/1D/stats.pdf"
-ggsave(file=stats_pdf, stats_save, width = 7, height = 3.5)
+ggsave(file=stats_pdf, stats_save, width = 6, height = 5.5)
 file.remove("Rplots.pdf")
 
 #### ONLY RELEVANT IF WE HAVE TO GO BY HAPLOTYPES ####
--- scripts/sort_time.sh
+++ scripts/sort_time.sh
@@ -2,9 +2,9 @@
 
 # GFA
 # !!!! BECAUSE OF ALIBI'S INTERESTING INVOCATION TACTICS, WE ALSO NEED TO SPECIFY THE FULL PATH !!!!
-#GG=/home/heumos/Downloads/hogwild_paper/1D/DRB1-3123.fa.gz.seqwish.gfa
+GG=/home/heumos/Downloads/hogwild_paper/1D/DRB1-3123.fa.gz.seqwish.gfa
 #GG=/home/heumos/git/pangenome/results_LPA/seqwish/LPA.fa.gz.seqwish.gfa
-GG=/home/heumos/git/pangenome/results_yeast/seqwish/cerevisiae.pan.fa.gz.seqwish.gfa
+#GG=/home/heumos/git/pangenome/results_yeast/seqwish/cerevisiae.pan.fa.gz.seqwish.gfa
 G=$(basename "$GG")
 GA=$(ls "$GG" | sed 's/.gfa/_sorted.gfa/g')
 # path to ALIBI executable
@@ -38,7 +38,7 @@ do
     /usr/bin/time --verbose odgi sort -i "$GG" -o odgi_Y/"$G".Y.og --threads "$T" -P -Y 2> odgi_Y/odgi_Y_time_"$i"
 done
 ### take a look at the last sorted graph
-odgi stats -i odgi_Y/"$G".Y.og -m > odgi_Y/"$G".Y.og.stats.yml
+odgi stats -i odgi_Y/"$G".Y.og -m -j -w -t "$T" > odgi_Y/"$G".Y.og.stats.yml
 
 ## odgi_s
 if [ ! -d odgi_s ]; then
@@ -50,7 +50,7 @@ do
     echo "odgi_s: $i"
     /usr/bin/time --verbose odgi sort -i "$GG" -o odgi_s/"$G".g.og --threads "$T" -P -p s 2> odgi_s/odgi_s_time_"$i"
 done
-odgi stats -i odgi_s/"$G".g.og -m > odgi_s/"$G".s.og.stats.yml
+odgi stats -i odgi_s/"$G".g.og -m -j -w -t "$T" > odgi_s/"$G".s.og.stats.yml
 
 ## odgi_Ygs
 if [ ! -d odgi_Ygs ]; then
@@ -62,7 +62,7 @@ do
     echo "odgi_Ygs: $i"
     /usr/bin/time --verbose odgi sort -i "$GG" -o odgi_Ygs/"$G".Ygs.og --threads "$T" -P -p Ygs 2> odgi_Ygs/odgi_Ygs_time_"$i"
 done
-odgi stats -i odgi_Ygs/"$G".Ygs.og -m > odgi_Ygs/"$G".Ygs.og.stats.yml
+odgi stats -i odgi_Ygs/"$G".Ygs.og -m -j -w -t "$T" > odgi_Ygs/"$G".Ygs.og.stats.yml
 
 ## alibi
 if [ ! -d alibi ]; then
@@ -79,7 +79,7 @@ do
     /usr/bin/time --verbose odgi sort -i "$GG" -s "$P"/alibi/"$G".alibi.order -P -o "$P"/alibi/"$G".alibi.og 2> "$P"/alibi/alibi_order_time_"$i"
 done
 cd "$P"
-odgi stats -i alibi/"$G".alibi.og -m > alibi/"$G".alibi.og.stats.yml
+odgi stats -i alibi/"$G".alibi.og -m -j -w -t "$T" > alibi/"$G".alibi.og.stats.yml
 
 if [ -f odgi_Y/odgi_Y_time.csv ] ; then
     rm odgi_Y/odgi_Y_time.csv
